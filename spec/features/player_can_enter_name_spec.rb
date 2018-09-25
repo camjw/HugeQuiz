1f2bb81dbb3ee7697cf6e412dee383b2831db2b2
Cameron Whitehead <camjw119@gmail.com>
2018-09-25 15:09:21 +0100

    Added player name ferature test

---
+++ spec/features/player_can_enter_name_spec.rb
@@ -0,0 +1,16 @@
+feature 'the user can enter their name and be greeted by it' do
+  scenario 'on loading the page' do
+    visit '/'
+    fill_in(:player_name, with: 'Billy')
+    click_button 'Play!'
+    expect(page).to have_content 'Welcome to the Quiz, Billy!'
+  end
+end
+
+feature 'the user can choose not to enter their name and still be greeted' do
+  scenario 'on loading the page' do
+    visit '/'
+    click_button 'Play!'
+    expect(page).to have_content 'Welcome to the Quiz, stranger!'
+  end
+end
