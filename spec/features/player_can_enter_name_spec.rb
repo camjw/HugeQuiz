90bcccac3f3fdb8d5665769534eefccb621a57cb
Cameron Whitehead <camjw119@gmail.com>
2018-09-26 16:21:28 +0100

    Refactored one test

--- spec/features/player_can_enter_name_spec.rb
+++ spec/features/player_can_enter_name_spec.rb
@@ -11,6 +11,6 @@ feature 'the user can choose not to enter their name and still be greeted' do
   scenario 'on loading the page' do
     visit '/'
     click_button 'Play!'
-    expect(page).to have_content 'Welcome to the Quiz, stranger!'
+    expect(page).to have_content 'Welcome to the Quiz, Stranger!'
   end
 end
